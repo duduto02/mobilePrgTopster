f9f028d30108deef7f06bfff8036e82e4cdfdefe
gang1yun <duduto02@naver.com>
2023-05-27 23:25:28 +0900

    05.27. ㅈ같네

--- app/src/main/java/com/example/topster/MainActivity.java
+++ app/src/main/java/com/example/topster/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.topster;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView1 = findViewById(R.id.imageView1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null) {
+
+            Uri photoUri = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),photoUri);
+            } catch (IOException e) { }
+
+            imageView1.setImageBitmap(bitmap);
+
+            Cursor cursor = getContentResolver().query(Uri.parse(data.getData().toString()));
+        }
+    }
+
+
+
 }
